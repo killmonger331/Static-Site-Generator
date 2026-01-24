8f73c6160472b6c4bcaa524bff23ff1b86d4c224
killmonger331 <richardmorales_24@hotmail.com>
2026-01-24 15:16:25 -0800

    Added LeafNode and test cases

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,3 +13,16 @@ class HTMLNode:
         return props_str
     def __repr__(self):
         return f'HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})'
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, **props):
+        super().__init__(tag=tag, value=value, children=[], **props)
+    def to_html(self):
+        if not self.value:
+            raise ValueError("LeafNode must have a value to convert to HTML")
+        if not self.tag:
+            return self.value
+        props_str = self.props_to_html()
+        return f'<{self.tag}{props_str}>{self.value}</{self.tag}>'
+    def __repr__(self):
+        return f'LeafNode(tag={self.tag}, value={self.value}, props={self.props})'
