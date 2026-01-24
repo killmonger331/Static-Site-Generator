678fa7ed09bef26eb3ae2121c762657e3d8566a3
killmonger331 <richardmorales_24@hotmail.com>
2026-01-24 13:10:57 -0800

    added Textnode and test cases

--- src/textnode.py
+++ src/textnode.py
@@ -11,7 +11,7 @@ class TextNode:
         self.text = text
         self.text_type = text_type
         self.url = url
-    def __eq__(TextNode, other):
+    def __eq__(self, other):
         return (self.text == other.text and
                 self.text_type == other.text_type and
                 self.url == other.url)
