a7fa4b5a9c3c315971bda3289e92ff74de45df0a
killmonger331 <richardmorales_24@hotmail.com>
2026-01-24 20:03:33 -0800

    added TextNode to HTMLNode conversion method

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,6 @@
 from enum import Enum
+
+from htmlnode import LeafNode
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
@@ -16,4 +18,21 @@ class TextNode:
                 self.text_type == other.text_type and
                 self.url == other.url)
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(node):
+    if node.text_type == TextType.TEXT:
+        return LeafNode(None, node.text)
+    elif node.text_type == TextType.BOLD:
+        return LeafNode("strong", node.text)
+    elif node.text_type == TextType.ITALIC:
+        return LeafNode("em", node.text)
+    elif node.text_type == TextType.CODE:
+        return LeafNode("code", node.text)
+    elif node.text_type == TextType.LINK:
+        return LeafNode("a", node.text, {"href": node.url})
+    elif node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": node.url, "alt": node.text})
+    else:
+        raise ValueError("Unknown TextType")
